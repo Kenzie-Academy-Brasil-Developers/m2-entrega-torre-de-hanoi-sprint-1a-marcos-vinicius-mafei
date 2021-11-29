09e12835c162559e4c04c174d3929adafba05466
LucasTatagiba <marlucetata@hotmail.com>
2021-11-29 18:42:19 -0300

    creating function that get and move the disk

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-function createGame(diskNumber = 3){
-    const game = document.querySelector(".game")
-    for(let towers = 0; towers < 3;towers++){
-    const tower = document.createElement("div")
-    if(towers === 0){
-        for(let i = 0; i < diskNumber; i++){
-            const disk = document.createElement("div")
-            disk.classList.add("disk"+(diskNumber-i))
-            disk.classList.add("disks")
-            tower.appendChild(disk)
+// cria os discos e as torres //
+
+const game = document.querySelector(".game")
+let currentDisk = 0
+
+function createGame(diskNumber = 3) {
+
+    for (let towers = 0; towers < 3; towers++) {
+        const tower = document.createElement("div")
+        if (towers === 0) {
+            for (let i = 0; i < diskNumber; i++) {
+                const disk = document.createElement("div")
+                disk.classList.add("disk" + (diskNumber - i))
+                disk.classList.add("disks")
+                tower.appendChild(disk)
+            }
         }
+        tower.classList.add("game--towers")
+        game.appendChild(tower)
     }
-    tower.classList.add("game--towers")
-    game.appendChild(tower)
-    }
+}
+
+createGame()
+
+// cria funcionalidades do jogo // 
+
+const tower1 = document.getElementsByClassName("game--towers")[0]
+const tower2 = document.getElementsByClassName("game--towers")[1]
+const tower3 = document.getElementsByClassName("game--towers")[2]
+
+tower1.addEventListener("click", getDisk)
+tower2.addEventListener("click", getDisk)
+tower3.addEventListener("click", getDisk)
+
+function getDisk(event) {
+    currentDisk = event.currentTarget.lastChild
+    tower1.removeEventListener("click", getDisk)
+    tower2.removeEventListener("click", getDisk)
+    tower3.removeEventListener("click", getDisk)
+    
+    tower1.addEventListener("click", moveDisk)
+    tower2.addEventListener("click", moveDisk)
+    tower3.addEventListener("click", moveDisk)
+}
+
+function moveDisk(event) {    
+    tower1.removeEventListener("click", moveDisk)
+    tower2.removeEventListener("click", moveDisk)
+    tower3.removeEventListener("click", moveDisk)
+    
+    tower1.addEventListener("click", getDisk)
+    tower2.addEventListener("click", getDisk)
+    tower3.addEventListener("click", getDisk)
+    event.currentTarget.appendChild(currentDisk)
 }
