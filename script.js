bcb515b95a8a355db25fc354ae23e2cf686e6efb
Marcos <marcosmafeimachado@gmail.com>
2021-11-30 14:51:08 -0300

    creating the decor dinamically as well

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const game = document.querySelector(".game")
 let currentDisk = 0
 let count = 0
-let movimentsCount = document.getElementsByClassName("game--grass")[0]
 const modalVitoria = document.getElementById('modalVitoria')
 const modalRegras = document.getElementById('modalRegras')
-const button = document.querySelector("button")
+const buttonReset = document.querySelectorAll("button")[3]
+const main = document.querySelector("main")
+const selectDificultPop = document.querySelector(".selectDificult")
+selectDificultPop.addEventListener("click", selectDificult)
 const defaultPosition = []
+const towersArray = []
+
 // cria os discos e as torres //
 function createGame(diskNumber = 3) {
 
@@ -21,45 +25,69 @@ function createGame(diskNumber = 3) {
             }
         }
         tower.classList.add("game--towers")
+        tower.addEventListener("click",getDisk)
+        towersArray.push(tower)
         game.appendChild(tower)
+    }
+    for(let i = 0; i <3;i++){
+        const div = document.createElement("div")
+        if(i === 0){
+            div.classList.add("game--bottom")
+        }else if(i === 1){
+            div.classList.add("game--grass")
+            div.innerText = "total movements: 0"
+        }else{
+            div.classList.add("game--ground")
+        }
+        main.appendChild(div)
     }
 }
 
-createGame()
+function selectDificult(event){
+    if(event.target.classList.contains("dificcult")){
+        let difficult = event.target.innerText
+        if(difficult==="Easy"){
+            createGame(3)
+        }else if(difficult==="Normal"){
+            createGame(4)
+        }else if(difficult==="Hard"){
+            createGame(5)
+        }
+        selectDificultPop.classList.add("displayNone")
+    }
+}
+
+// createGame()
 
 // cria funcionalidades do jogo // 
 
-const tower1 = document.getElementsByClassName("game--towers")[0]
-const tower2 = document.getElementsByClassName("game--towers")[1]
-const tower3 = document.getElementsByClassName("game--towers")[2]
+// const tower1 = document.getElementsByClassName("game--towers")[0]
+// const tower2 = document.getElementsByClassName("game--towers")[1]
+// const tower3 = document.getElementsByClassName("game--towers")[2]
 
-tower1.addEventListener("click", getDisk)
-tower2.addEventListener("click", getDisk)
-tower3.addEventListener("click", getDisk)
-button.addEventListener("click", reset)
+// tower1.addEventListener("click", getDisk)
+// tower2.addEventListener("click", getDisk)
+// tower3.addEventListener("click", getDisk)
+buttonReset.addEventListener("click", reset)
 
 function getDisk(event) {
     currentDisk = event.currentTarget.lastChild
-    tower1.removeEventListener("click", getDisk)
-    tower2.removeEventListener("click", getDisk)
-    tower3.removeEventListener("click", getDisk)
-    tower1.addEventListener("click", moveDisk)
-    tower2.addEventListener("click", moveDisk)
-    tower3.addEventListener("click", moveDisk)
+    for(let i = 0;i<towersArray.length;i++){
+        towersArray[i].removeEventListener("click", getDisk)
+        towersArray[i].addEventListener("click",moveDisk)
+    }
 }
 
 
 function moveDisk(event) {
-    tower1.removeEventListener("click", moveDisk)
-    tower2.removeEventListener("click", moveDisk)
-    tower3.removeEventListener("click", moveDisk)
-    tower1.addEventListener("click", getDisk)
-    tower2.addEventListener("click", getDisk)
-    tower3.addEventListener("click", getDisk)
+    for(let i = 0;i<towersArray.length;i++){
+        towersArray[i].addEventListener("click", getDisk)
+        towersArray[i].removeEventListener("click",moveDisk)
+    }
     if(event.currentTarget.childNodes.length === 0 || currentDisk.offsetWidth < event.currentTarget.lastChild.offsetWidth){
     event.currentTarget.appendChild(currentDisk)
     count++
-    movimentsCount.innerText =`Total de movimentos: ${count}`
+    document.querySelector(".game--grass").innerText =`Total de movimentos: ${count}`
     }else if(currentDisk.offsetWidth > event.currentTarget.lastChild.offsetWidth){
         modalRegras.classList.remove("displayNone")
         function hide(){
@@ -67,16 +95,16 @@ function moveDisk(event) {
         }
         setTimeout(hide,3000)
     }
-    if(tower3.childNodes.length === 3){
+    if(towersArray[2].childNodes.length === 3){
         modalVitoria.classList.remove("displayNone")
     }
 }
 
 function reset(){
     for(let i=0;i<defaultPosition.length;i++){
-        tower1.appendChild(defaultPosition[i])
+        towersArray[0].appendChild(defaultPosition[i])
     }
     modalVitoria.classList.add("displayNone")
     count = 0
-    movimentsCount.innerText =`Total de movimentos: ${count}`
+    document.querySelector(".game--grass").innerText =`Total de movimentos: ${count}`
 }
